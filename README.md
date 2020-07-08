ee627c342230e6e637db1db9bdc9653d7feb0ece
Florian Brezina <florian.brezina@live.de>
2020-07-08 18:24:17 +0200

    added methods to store credentials and be prompted for credentials

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ A set of Powershell scripts that simplify downloading data from GENESIS, the Ger
 1. `-table`: the code for the table you want to download, which has the format "XXXXX-XXXX" in DESTATIS`s GENESIS database. Put the code in double-quotes.
 2. `-file` (optional): the filename of the output file. The default uses the table code "XXXXX-XXXX.csv" in your current directory.
 3. `-cred` (optional): the filename of your credentials file. The default assumes "destatis-credentials.json" in your current directory.
+4. `-promptCredentials` (optional): instead of using stored credentials, you can choose to provide them interactively.
+4. `-saveCredentials` (optional): a method that prompts for credentials and stores them in the provided `cred` destination. No data will be downloaded.
 4. `-format` (optional): the file format of the output file. The default uses the tidy `ffcsv` format. Other options are `csv`,  `datencsv`, `xlsx` and `html`.
 
 ## Binaries
--- src/Get-Genesis-Table.ps1
+++ src/Get-Genesis-Table.ps1
@@ -1,15 +1,34 @@
-param ($table, $file, $cred, $format='ffcsv')
+param (
+[string]$table, 
+[string]$file, 
+[string]$cred='.\destatis-credentials.json', 
+[string]$format='ffcsv',
+[Parameter(Mandatory=$false)]
+[switch]$promptCredentials,
+[Parameter(Mandatory=$false)]
+[switch]$saveCredentials
+)
 if ($file -eq $null) {
-$file = ".\$($table).csv" 
+    $file = ".\$($table).csv" 
 }
-if ($cred -eq $null) {
-    $cred = ".\genesis-credentials.json" 
+
+# credentials
+if($promptCredentials -or $saveCredentials -or (-not (Test-Path $cred))){
+    $username=Read-Host -Prompt 'Enter your DESTATIS user name'
+    $password=Read-Host -Prompt 'Enter your DESTATIS password'
+    if($saveCredentials){
+        @{username=$username;password=$password} | ConvertTo-Json | Out-File $cred
+        Exit
+    }
+} else {
+    $cred = Get-Content $cred | ConvertFrom-Json
+    $username=$cred.username
+    $password=$cred.password
 }
-$cred = Get-Content $cred | ConvertFrom-Json
+
+# request
 $baseurl="https://www-genesis.destatis.de/genesisWS/rest/2020"
 $service="data"
 $method="tablefile"
-$username=$cred.username
-$password=$cred.password
 $url="$($baseurl)/$($service)/$($method)?username=$username&password=$password&name=$table&format=$format"
 Invoke-RestMethod -Method 'Get' -Uri $url -OutFile $file
