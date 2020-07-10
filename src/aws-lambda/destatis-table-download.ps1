bbf3d2b1e98144a4cbd2bb17acbcc55781d89e8e
Florian Brezina <florian.brezina@live.de>
2020-07-10 09:48:09 +0200

    finalized aws version

--- src/aws-lambda/destatis-table-download.ps1
+++ src/aws-lambda/destatis-table-download.ps1
@@ -1,43 +1,24 @@
-# PowerShell script file to be executed as a AWS Lambda function. 
-# 
-# When executing in Lambda the following variables will be predefined.
-#   $LambdaInput - A PSObject that contains the Lambda function input data.
-#   $LambdaContext - An Amazon.Lambda.Core.ILambdaContext object that contains information about the currently running Lambda environment.
-#
-# The last item in the PowerShell pipeline will be returned as the result of the Lambda function.
-#
-# To include PowerShell modules with your Lambda function, like the AWS.Tools.S3 module, add a "#Requires" statement
-# indicating the module and version. If using an AWS.Tools.* module the AWS.Tools.Common module is also required.
-
+# dependencies
 #Requires -Modules @{ModuleName='AWS.Tools.Common';ModuleVersion='4.0.6.0'}
 #Requires -Modules @{ModuleName='AWS.Tools.Lambda';ModuleVersion='4.0.6.0'}
-<<<<<<< HEAD
 #Requires -Modules @{ModuleName='AWS.Tools.S3';ModuleVersion='4.0.6.0'}
 
+# variables from environment
 $format=$env:format
 $table=$env:table
 $username=$env:username
 $password=$env:password
+$s3bucket=$env:s3bucket
+$s3key=$env:s3key
 
-# request
-$file="$($table).csv" 
-=======
-
-
-$username="DEH4E2B8WT"
-$password="WdyKa6ZYyVSp89Y"
-$format="ffcsv"
-$table="12411-0001"
-
-# request
->>>>>>> e5a048037d3aa1acaf8cd0915016350e867b89eb
+# build the url
 $baseurl="https://www-genesis.destatis.de/genesisWS/rest/2020"
 $service="data"
 $method="tablefile"
 $url="$($baseurl)/$($service)/$($method)?username=$username&password=$password&name=$table&format=$format"
-<<<<<<< HEAD
+
+# request
 $response=Invoke-RestMethod -Method 'Get' -Uri $url
-Write-S3Object -BucketName "data-destatis" -Key $file -Content $response
-=======
-Invoke-RestMethod -Method 'Get' -Uri $url -OutFile $file
->>>>>>> e5a048037d3aa1acaf8cd0915016350e867b89eb
+
+# upload to S3
+Write-S3Object -BucketName $s3bucket -Key $s3key -Content $response
